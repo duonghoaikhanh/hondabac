6dcd0684c526e35acd6bf60f428718e8082fa8d3
duonghoaikhanh <duonghoaikhanh@gmail.com>
2016-01-04 19:13:16 +0700

    first commit

---
+++ dbcon.php
@@ -0,0 +1,11 @@
+<?php
+$conf = array();
+$conf['host']='localhost';
+$conf['dbuser']='root';
+$conf['dbpass']='';
+$conf['dbname']='hondabac';
+$conf['rooturl']='http://'.$_SERVER['HTTP_HOST'].'/hondabac/';
+$conf['rootpath']=$_SERVER['DOCUMENT_ROOT'].'/hondabac/';
+$conf['rooturl_web']='http://'.$_SERVER['HTTP_HOST'].'/hondabac/';
+$conf['rootpath_web']=$_SERVER['DOCUMENT_ROOT'].'/hondabac/';
+?>
